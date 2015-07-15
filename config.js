9bf023ae6f5df168457a8bda8e168a5067d69aa4
André Santos <andrefs@cpan.org>
2015-07-15 23:18:24 +0100

    update

---
+++ config.js
@@ -0,0 +1,30 @@
+exports.config = {
+    project:{
+        title         : 'Instant Beatriz Gosta',
+        description   : 'As melhores frases de Beatriz Gosta!',
+        url           : 'http://beatriz.botoes.co',
+        external_link : 'http://www.facebook.com/beatrizgosta',
+        facebook: {
+            id: "1016048086"
+        },
+        google: {
+            id: 'UA-64812624-1'
+        },
+    },
+    partials: {
+        footer: '<a href="mailto:andrefs@andrefs.com">andré santos, joão ferreira <i class="fa fa-envelope"></i></a> , baseado em <a href="https://www.youtube.com/channel/UC3tGjmFxqHfsUGaQkFjaUpA">beatriz gosta <i class="fa fa-external-link"></i></a>'
+    },
+    manifest : {
+        version: 'v1',
+        filePath: './',
+        fileName: 'manifest.json'
+    },
+    sourcesPath       : './sources',
+    soundsPath        : './sounds',
+    tmpPath           : './tmp',
+    defaultClipMargin : '2',
+    soundEditCommand  : 'audacity',
+    server: {
+        port: 10111
+    }
+};
